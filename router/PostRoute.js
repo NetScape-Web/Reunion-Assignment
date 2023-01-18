f5508c87aab90ee9d5aabfb04aeb0b417c028da2
Sonu Kumar <sonu.patna0808@gmail.com>
2023-01-18 16:14:25 +0530

    Adding Delete Posts by id Route

--- router/PostRoute.js
+++ router/PostRoute.js
@@ -39,4 +39,29 @@ router.post("/", async (req, res) => {
   }
 });
 
+// delete post
+router.delete("/:id", async (req, res) => {
+  const { user } = req;
+  const { id: postId } = req.params;
+  const post = await Post.findById(postId);
+  if (user.id.toString() === post.createdBy.toString()) {
+    try {
+      await Post.findByIdAndDelete(post._id);
+      return res.status(200).json({
+        error: false,
+        message: "Post deleted",
+      });
+    } catch (error) {
+      return res.status(404).json({
+        error: true,
+        message: "Something went wrong.",
+      });
+    }
+  }
+  return res.status(401).json({
+    error: true,
+    message: "You are not authorized to delete this post.",
+  });
+});
+
 export default router;
